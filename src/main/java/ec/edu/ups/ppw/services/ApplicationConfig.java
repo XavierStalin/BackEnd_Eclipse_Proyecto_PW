53f7df051db65054c702471fc16b600ef5ca212d
Xavier Stalin Siguachi <115356779+XavierStalin@users.noreply.github.com>
2026-02-05 18:06:41 -0500

    Add ApplicationConfig for JAX-RS API configuration

---
+++ src/main/java/ec/edu/ups/ppw/services/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package ec.edu.ups.ppw.services;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+}
